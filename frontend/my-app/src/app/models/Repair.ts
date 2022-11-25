eda203b7901ba2b7f319c469ed43748d4da3dec0
Dylaixo <marcin.log01@gmail.com>
2022-11-25 23:49:13 +0100

    get i seter

--- frontend/my-app/src/app/models/Repair.ts
+++ frontend/my-app/src/app/models/Repair.ts
@@ -3,8 +3,6 @@ import { Worker } from './Worker';
 import { Car } from './Car';
 import { Client } from './Client';
 export class Repair {
-
-
   constructor(
     private _client: Client,
     private _worker: Worker,
@@ -15,37 +13,53 @@ export class Repair {
     private _during: boolean,
     private _price: number
   ) { }
-  public get price(): number {
-    return this._price;
-  }
-
-  public get during(): boolean {
-    return this._during;
-  }
-
-  public get done(): boolean {
-    return this._done;
-  }
-
-  public get endTime(): Date {
-    return this._endTime;
-  }
-
-  public get createTime(): Timestamp<any> {
-    return this._createTime;
-  }
-
-  public get car(): Car {
-    return this._car;
-  }
-
-  public get worker(): Worker {
-    return this._worker;
-  }
-
   public get client(): Client {
     return this._client;
   }
-
+  public set client(value: Client) {
+    this._client = value;
+  }
+  public get price(): number {
+    return this._price;
+  }
+  public set price(value: number) {
+    this._price = value;
+  }
+  public get during(): boolean {
+    return this._during;
+  }
+  public set during(value: boolean) {
+    this._during = value;
+  }
+  public get done(): boolean {
+    return this._done;
+  }
+  public set done(value: boolean) {
+    this._done = value;
+  }
+  public get endTime(): Date {
+    return this._endTime;
+  }
+  public set endTime(value: Date) {
+    this._endTime = value;
+  }
+  public get createTime(): Timestamp<any> {
+    return this._createTime;
+  }
+  public set createTime(value: Timestamp<any>) {
+    this._createTime = value;
+  }
+  public get car(): Car {
+    return this._car;
+  }
+  public set car(value: Car) {
+    this._car = value;
+  }
+  public get worker(): Worker {
+    return this._worker;
+  }
+  public set worker(value: Worker) {
+    this._worker = value;
+  }
 }
 
