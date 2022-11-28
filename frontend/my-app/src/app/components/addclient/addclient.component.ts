40da66dd50d52a688280e908bb008031a9d76195
Dylaixo <marcin.log01@gmail.com>
2022-11-28 16:02:20 +0100

    sendClient

--- frontend/my-app/src/app/components/addclient/addclient.component.ts
+++ frontend/my-app/src/app/components/addclient/addclient.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClientListApicallService } from 'src/app/services/client-list-apicall.service';
+import { Client } from 'src/app/models/Client';
 @Component({
   selector: 'app-addclient',
   templateUrl: './addclient.component.html',
   styleUrls: ['./addclient.component.css']
 })
 export class AddclientComponent implements OnInit {
-
-  constructor() { }
+  client = new Client("","","","",0)
+  constructor(private clientService: ClientListApicallService) { }
 
   ngOnInit(): void {
   }
-
+  sendClient(client : Client){
+    this.clientService.sendClient(client,'POST');
+  }
 }
