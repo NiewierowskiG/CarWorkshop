812c4c6e99ac682aa7c12e026d0c04acad17a402
Dylaixo <marcin.log01@gmail.com>
2022-11-29 13:04:36 +0100

    front tabelki

--- frontend/my-app/src/app/components/client-list/client-list.component.ts
+++ frontend/my-app/src/app/components/client-list/client-list.component.ts
@@ -3,11 +3,6 @@ import { Observable, timeInterval } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { ClientListApicallService } from '../../services/client-list-apicall.service';
 import { Client } from 'src/app/models/Client';
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 47373a2ac7aa50a0cb5d19d274d19af121815e3c
 @Component({
   selector: 'app-client-list',
   templateUrl: './client-list.component.html',
@@ -22,15 +17,6 @@ export class ClientListComponent implements OnInit {
 
   ngOnInit(): void {
     this.clients$ = this.clientService.getClient();
-  }
-<<<<<<< HEAD
-  sendClient(client : Client){
-    this.clientService.sendClient(client,"POST");
-=======
-
-  sendClient(client: Client): void {
-    this.clientService.postClient(client, "POST");
->>>>>>> 47373a2ac7aa50a0cb5d19d274d19af121815e3c
   }
 
   delClient(id: number): void {
