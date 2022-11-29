47373a2ac7aa50a0cb5d19d274d19af121815e3c
Filip <wareewaa@gmail.com>
2022-11-29 09:31:48 +0100

    delWorker, addrepair fix

--- frontend/my-app/src/app/components/worker-list/worker-list.component.ts
+++ frontend/my-app/src/app/components/worker-list/worker-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Worker } from 'src/app/models/Worker';
 import { WorkerListApicallService } from 'src/app/services/worker-list-apicall.service';
+import {Observable} from "rxjs";
 @Component({
   selector: 'app-worker-list',
   templateUrl: './worker-list.component.html',
   styleUrls: ['./worker-list.component.css']
 })
 export class WorkerListComponent implements OnInit {
+  workers$?: Observable<Worker[]>
   workers?: Worker[]
   searchText?: string;
   constructor(private workerService: WorkerListApicallService) { }
@@ -18,6 +20,10 @@ export class WorkerListComponent implements OnInit {
     this.workerService.getWorker().subscribe(workers =>{
       this.workers = workers
     })
+  }
+  delWorker(id: number): void {
+    this.workerService.delWorker(id);
+    setTimeout( (_:any)=>{this.workers$ = this.workerService.getWorker();} ,10) //time to be sure that client was deleted
   }
   getLink(salary:number,id:number){
     return `/EditWorker/`.concat(salary.toString(), '/', id.toString());
