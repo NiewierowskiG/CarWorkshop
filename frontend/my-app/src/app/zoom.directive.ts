b5bd12f88648f0769c88c95a1537024360180bb6
Dylaixo <marcin.log01@gmail.com>
2022-12-14 21:22:58 +0100

    style strony

--- frontend/my-app/src/app/zoom.directive.ts
+++ frontend/my-app/src/app/zoom.directive.ts
@@ -9,19 +9,21 @@ export class ZoomDirective  {
   }
 
   @HostListener('mouseenter') onMouseEnter(){
-    this.zoom(1.05, 'lightgrey')
+    this.zoom(1.02, 'dodgerblue', 'white')
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.zoom(1.0, '')
+    this.zoom(1.0, '','')
   }
   private highlight(color: string) {
   this.element.nativeElement.style.backgroundColor = color;
 }
 
-  private zoom(amount: number, color: string){
+  private zoom(amount: number, color: string, fcolor: string  ){
     this.element.nativeElement.style.transform = `scale(${amount})`;
     this.element.nativeElement.style.backgroundColor = color;
+    this.element.nativeElement.style.color = fcolor;
+    this.element.nativeElement.style.transition = `1s linear`
   }
 
 }
