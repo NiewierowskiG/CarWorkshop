d60e3b0584110b7011949725f56fde40f6b30a3e
wareeewaa <wareeewaa@gmail.com>
2022-12-15 03:30:32 +0100

    walidacja przy usuwaniu workera i clienta z istniejacymi naprawami

--- frontend/my-app/src/app/components/client-list/client-list.component.ts
+++ frontend/my-app/src/app/components/client-list/client-list.component.ts
@@ -3,6 +3,9 @@ import { Observable, timeInterval } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { ClientListApicallService } from '../../services/client-list-apicall.service';
 import { Client } from 'src/app/models/Client';
+import { RepairListApicallService } from 'src/app/services/repair-list-apicall.service';
+import {Repair} from "../../models/Repair";
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-client-list',
   templateUrl: './client-list.component.html',
@@ -12,16 +15,35 @@ export class ClientListComponent implements OnInit {
   searchText?: string;
   clients$?: Observable<Client[]>
 
+  repairs?: Repair[];
+  repairs$?: Observable<Repair[]>
+  constructor(private clientService: ClientListApicallService, private repairService: RepairListApicallService, private clientHttp: HttpClient) {}
 
-  constructor(private clientService: ClientListApicallService) { }
 
   ngOnInit(): void {
     this.clients$ = this.clientService.getClient();
+    this.repairs$ = this.repairService.getRepair();
+    this.clientHttp.get<Repair[]>("http://localhost:8000/repairs.json").subscribe(repairs => {
+      this.repairs = repairs;
+      console.log(repairs);
+    });
   }
 
-  delClient(id: number): void {
-    this.clientService.delClient(id);
-    setTimeout( (_:any)=>{this.clients$ = this.clientService.getClient();} ,10) //time to be sure that client was deleted
+  delClient(id: number, email: string): void {
+    if(this.repairs != undefined)
+    {
+      let filtr = this.repairs.filter(repair => repair.client.email == email)
+      console.log(filtr)
+      if (filtr.length > 0) {
+        alert("Naprawa istnieje")
+      }
+      else
+      {
+        this.clientService.delClient(id);
+        setTimeout( (_:any)=>{this.clients$ = this.clientService.getClient();} ,10); //time to be sure that client was deleted
+      }
+    }
+
   }
 
   putClient(client: Client, id: number) {
--- frontend/my-app/src/app/components/worker-list/worker-list.component.ts
+++ frontend/my-app/src/app/components/worker-list/worker-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Worker } from 'src/app/models/Worker';
 import { WorkerListApicallService } from 'src/app/services/worker-list-apicall.service';
 import {Observable} from "rxjs";
+import {Repair} from "../../models/Repair";
+import {RepairListApicallService} from "../../services/repair-list-apicall.service";
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-worker-list',
   templateUrl: './worker-list.component.html',
@@ -11,19 +14,38 @@ export class WorkerListComponent implements OnInit {
   workers$?: Observable<Worker[]>
   workers?: Worker[]
   searchText?: string;
-  constructor(private workerService: WorkerListApicallService) { }
+  repairs?: Repair[];
+  repairs$?: Observable<Repair[]>
+  constructor(private workerService: WorkerListApicallService, private repairService: RepairListApicallService, private workerHttp: HttpClient) { }
 
   ngOnInit(): void {
     this.getWorker()
+    this.repairs$ = this.repairService.getRepair();
+    this.workerHttp.get<Repair[]>("http://localhost:8000/repairs.json").subscribe(repairs => {
+      this.repairs = repairs;
+      console.log(repairs);
+    });
   }
   getWorker(){
     this.workerService.getWorker().subscribe(workers =>{
       this.workers = workers
     })
   }
-  delWorker(id: number): void {
-    this.workerService.delWorker(id);
-    setTimeout( (_:any)=>{ this.getWorker();} ,10) //time to be sure that client was deleted
+  delWorker(id: number, email: string): void {
+    if (this.repairs != undefined)
+    {
+      let filtr = this.repairs.filter(repair => repair.worker.email == email)
+      console.log(filtr)
+      if (filtr.length > 0)
+      {
+        alert("Naprawa istnieje")
+      }
+      else
+      {
+        this.workerService.delWorker(id);
+        setTimeout((_: any) => {this.getWorker();}, 10) //time to be sure that client was deleted
+      }
+    }
   }
   getLink(salary:number,id:number){
     return `/EditWorker/`.concat(salary.toString(), '/', id.toString());
