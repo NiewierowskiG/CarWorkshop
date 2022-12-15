88f730780b1a4fe333036cb0b2a4cca76b65fca1
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-12-15 02:30:12 +0100

    money earned worker

--- frontend/my-app/src/app/components/editworker/editworker.component.ts
+++ frontend/my-app/src/app/components/editworker/editworker.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Worker } from "../../models/Worker";
 import { WorkerListApicallService } from "../../services/worker-list-apicall.service";
 
@@ -9,18 +11,24 @@ import { WorkerListApicallService } from "../../services/worker-list-apicall.ser
   styleUrls: ['./editworker.component.css']
 })
 export class EditworkerComponent implements OnInit {
-  id?: number
+  id!: number
   salary?: number
-  constructor(private route: ActivatedRoute, private workerservice: WorkerListApicallService) { }
+  money?: number
+  constructor(private route: ActivatedRoute, private workerservice: WorkerListApicallService,private client: HttpClient) { }
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get("id"));
     this.salary = Number(this.route.snapshot.paramMap.get("salary"));
+
+    this.client.get<any>("http://localhost:8000/workerMoney/".concat(this.id.toString())).subscribe(money => {
+      this.money = money.amount;
+    });
   }
   putWorker( salary?: number,id?: number):void{
     if(id!=undefined && salary!=undefined ){
       this.workerservice.putWorker(salary,id);
     }
   }
+
 }
 
