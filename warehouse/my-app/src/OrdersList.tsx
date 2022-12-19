ffba33065ffcf3890e1dbd337afdab0815ffc3af
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-12-19 01:06:00 +0100

    basic 2 components

---
+++ warehouse/my-app/src/OrdersList.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import './OrdersList.css'
+
+interface Order{
+    id: number;
+    items_count: number;
+    date: string;
+    title: string;
+}
+
+interface Props {
+    orders: Order[]
+}
+
+interface State {
+  // State type definition goes here
+}
+
+class OrdersList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+          {
+              <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Items</th>
+            <th>Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.orders.map(order => (
+            <tr key={order.id}>
+              <td>{order.id}</td>
+              <td>{order.items_count}</td>
+              <td>{order.date}</td>
+              <td>{order.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+          }
+      </div>
+    );
+  }
+}
+
+export default OrdersList;
--- warehouse/my-app/src/index.js
+++ warehouse/my-app/src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import OrdersList from "./OrdersList.tsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const orders1 = [
+      {
+        id: 1,
+        items_count: 2,
+        date: '2022-01-01',
+        title: 'Order #1'
+      },
+      {
+        id: 2,
+        items_count: 3,
+        date: '2022-01-02',
+        title: 'Order #2'
+      }
+    ];
+const orders2 = [
+  {
+    id: 23478637,
+    items_count: 77,
+    date: '2021-09-14T15:39:01.319Z',
+    title: 'Ergonomic Plastic Chicken'
+  },
+  {
+    id: 84511239,
+    items_count: 57,
+    date: '2021-09-14T15:39:01.319Z',
+    title: 'Handcrafted Frozen Chips'
+  },
+  {
+    id: 58772912,
+    items_count: 73,
+    date: '2021-09-14T15:39:01.319Z',
+    title: 'Small Fresh Car'
+  },
+  {
+    id: 77114799,
+    items_count: 83,
+    date: '2021-09-14T15:39:01.319Z',
+    title: 'Intelligent Soft Soap'
+  },
+  {
+    id: 89627201,
+    items_count: 56,
+    date: '2021-09-14T15:39:01.319Z',
+    title: 'Gorgeous Concrete Towels'
+  }
+]
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+     <div className="OrdersList">
+         <OrdersList orders={orders1}/>
+     </div>
+      <div className="OrdersList">
+         <OrdersList orders={orders2}/>
+     </div>
+      <div className="OrdersList">
+         <OrdersList orders={orders1}/>
+     </div>
+      <div className="OrdersList">
+         <OrdersList orders={orders2}/>
+     </div>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
