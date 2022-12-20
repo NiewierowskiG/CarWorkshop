a58aa1dfb4bc37455e55a70bba5725442ad99117
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-12-20 01:04:03 +0100

    next console eror fix (createRoot)

--- warehouse/my-app/src/index.js
+++ warehouse/my-app/src/index.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./components/App/App";
 
-ReactDOM.render(
+
+let rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
       <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
