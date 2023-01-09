db0ad13a9043503fef9a1659c3416228306acfcf
GrzegorzNiewierowski <gniewierowski@gmail.com>
2023-01-09 17:54:22 +0100

    quick fix

--- backend/warsztat/email.py
+++ backend/warsztat/email.py
@@ -2,18 +2,15 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-class nazwa:
-    def __init__(self):
-        self.login = 'zespolowetest@wp.pl'
-        self.password = 'zespolowe#test21153!!!'
 
-
-    def send_email(email, subject, body):
-        msg = MIMEText(body, 'plain', 'utf-8')
-        msg['Subject'] = Header(subject, 'utf-8')
-        msg['From'] = self.login
-        msg['To'] = email
-        smtp = smtplib.SMTP_SSL("smtp.wp.pl", 465)
-        smtp.login(login, password)
-        smtp.sendmail(login, email, msg.as_string())
-        smtp.close()
+def send_email(email, subject, body):
+    password = 'zespolowe#test21153!!!'
+    login = 'zespolowetest@wp.pl'
+    msg = MIMEText(body, 'plain', 'utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = login
+    msg['To'] = email
+    smtp = smtplib.SMTP_SSL("smtp.wp.pl", 465)
+    smtp.login(login, password)
+    smtp.sendmail(login, email, msg.as_string())
+    smtp.close()
