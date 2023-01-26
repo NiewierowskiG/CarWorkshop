5bafecc8506953842bd6a33387fdb20404ee1337
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2023-01-26 16:10:53 +0100

    delete useless thinks

--- warehouse/my-app/src/components/App/App.test.tsx
+++ warehouse/my-app/src/components/App/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ShallowWrapper, shallow } from 'enzyme';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('axios', async () => {
   let { AUTH_TOKEN } = await import('../Config/Config');
@@ -19,7 +20,7 @@ jest.mock('axios', async () => {
 
 describe('<App/>', () => {
    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-   let appScreen: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>> | undefined;
+   let appScreen: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
    let appInstance;
    beforeEach(() => {
       appScreen = shallow(<App />);
@@ -28,12 +29,16 @@ describe('<App/>', () => {
    });
 
    afterEach(() => {
-      appScreen = undefined;
-      appInstance = undefined;
    });
 
    it('renders', () => {
       expect(appScreen!==undefined).toBe(true);
    });
+
+   it('renders static structure', () => {
+      expect(appScreen.first().type()).toBe(BrowserRouter);
+   });
+
+   
 
 });
